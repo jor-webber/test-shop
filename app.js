238ed08d9330db6318caa0b8d27f39f0506775f8
Jordan Webber <webber_jordan_95@hotmail.com>
2021-07-05 11:34:53 -0400

    Added MVC changes to the application

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+const express = require('express');
+const { urlencoded } = require('body-parser');
+const errorController = require('./controllers/error');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
