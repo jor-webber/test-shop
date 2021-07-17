ae8721564fd674e4a621677d96313bd4aefbb9be
Jordan Webber <webber_jordan_95@hotmail.com>
2021-07-16 22:56:16 -0400

    Added SQL into project

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,9 +13,18 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  req.user.createProduct({ 
+    title: title,
+    price: price,
+    imageUrl: imageUrl,
+    description: description,
+  }).then(result => {
+    //console.log(result);
+    console.log('Create Product');
+    res.redirect('/admin/products');
+  }).catch(err => {
+    console.log(err);
+  });
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -24,16 +33,20 @@ exports.getEditProduct = (req, res, next) => {
     res.redirect('/');
   }
   const productId = req.params.productId
-  Product.findById(productId, product => {
-    if (!product) {
-      return res.redirect('/');
-    }
-    res.render('admin/edit-product', {
-      pageTitle: 'Edit Product',
-      path: '/admin/edit-product',
-      editing: editMode,
-      product: product
-    });
+  req.user.getProducts({where: {id: productId}})
+    .then(products => {
+      const product = products[0];
+      if (!product) {
+        return res.redirect('/');
+      }
+      res.render('admin/edit-product', {
+        pageTitle: 'Edit Product',
+        path: '/admin/edit-product',
+        editing: editMode,
+        product: product
+      });
+  }).catch(err => {
+    console.log(err);
   });
 };
 
@@ -43,29 +56,42 @@ exports.postEditProduct = (req, res, next) => {
   const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
   const updatedDescription = req.body.description;
-  const updatedProduct = new Product(
-    productId, 
-    updatedTitle, 
-    updatedImageUrl, 
-    updatedDescription,
-    updatedPrice
-  );
-  updatedProduct.save();
-  res.redirect('/admin/products');
+  Product.findByPk(productId)
+    .then(product => {
+      product.title = updatedTitle;
+      product.price = updatedPrice;
+      product.description = updatedDescription;
+      product.imageUrl = updatedImageUrl;
+      return product.save();
+    })
+    .then(result => {
+      console.log('Updated Product!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
+  req.user.getProducts().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products',
     });
-  });
+  }).catch(err => {
+    console.log(err);
+  })
 }
 
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
-  Product.deleteById(productId);
-  res.redirect('/admin/products');
+  Product.findByPk(productId)
+    .then(product => {
+      return product.destroy();
+    })
+    .then(result => {
+      console.log('Destoryed Product!');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
--- util/database.js
+++ util/database.js
@@ -1,10 +1,8 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'Password1'
+const sequelize = new Sequelize('node-complete', 'root', 'Password1', {
+  dialect: 'mysql',
+  host: 'localhost'
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
